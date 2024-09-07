0562fdc48a6a2d73631a8544081fc4c56f8d2498
Blockchain and ai - Sheldon <stoneysheldon2@gmail.com>
2024-09-07 11:27:11 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,57 @@
+# Bring in deps
+import os 
+from apikey import apikey 
+
+import streamlit as st 
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain 
+from langchain.memory import ConversationBufferMemory
+from langchain.utilities import SupplyChainAPIWrapper
+
+os.environ['OPENAI_API_KEY'] = apikey
+
+# App framework
+st.title('🦜🔗 AI-Based Supply Chain Optimizer')
+prompt = st.text_input('Plug in your prompt here') 
+
+# Prompt templates
+title_template = PromptTemplate(
+    input_variables = ['topic'], 
+    template='Generate a title for optimizing the supply chain for this business scenario: {topic}'
+)
+
+script_template = PromptTemplate(
+    input_variables = ['title', 'wikipedia_research'], 
+    template='Create a supply chain optimization plan based on this title: {title}. Use this business data for optimization: {wikipedia_research} '
+)
+
+# Memory 
+title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
+
+
+# Llms
+llm = OpenAI(temperature=0.9) 
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=script_memory)
+
+wiki = SupplyChainAPIWrapper()
+
+# Show stuff to the screen if there's a prompt
+if prompt: 
+    title = title_chain.run(prompt)
+    wiki_research = wiki.run(prompt) 
+    script = script_chain.run(title=title, wikipedia_research=wiki_research)
+
+    st.write(title) 
+    st.write(script) 
+
+    with st.expander('Title History'): 
+        st.info(title_memory.buffer)
+
+    with st.expander('Script History'): 
+        st.info(script_memory.buffer)
+
+    with st.expander('Wikipedia Research'): 
+        st.info(wiki_research)
